f14cbaf0948162d7673032cbcd073a98d235ba19
Dayne May <dmay@beyondtrust.com>
2018-03-01 20:37:03 -0400

    Add basic speed-select component

---
+++ src/app/speed-select/speed-select.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'speed-select',
+  templateUrl: './speed-select.component.html',
+  styleUrls: ['./speed-select.component.less']
+})
+export class SpeedSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
